c5bb82d0ceb93f9cbb0d34ed38edc369d367f403
john-robinson-git <john.robinson.engineering@gmail.com>
2023-01-14 15:48:55 -0500

    pieces from the test files compiled into a main file

--- sheetsOffAlarmClock/sheetsOffAlarmClock.ino
+++ sheetsOffAlarmClock/sheetsOffAlarmClock.ino
@@ -1,40 +1,86 @@
-// Syren motor driver library Copyright (c) 2012 Dimension Engineering LLC
-// RTC Lbrary Copyright (c) Adafruit
+/*  Syren motor driver library Copyright (c) 2012 Dimension Engineering LLC
+ *  RTC Lbrary Copyright (c) Adafruit
+ *  Keypad and LCD Code sourced from Krishna Pattabiraman's how to set up a keypad on ardunio
+ *  located at https://www.circuitbasics.com/how-to-set-up-a-keypad-on-an-arduino/
+ */
 
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
 #include <SoftwareSerial.h>
 #include <SyRenSimplified.h>
+#include <RTClib.h>
 
+RTC_DS1307 rtc;
 SoftwareSerial SWSerial1(NOT_A_PIN, 1); // RX on no pin (unused), TX on pin 1 (to S1).
 SyRenSimplified SR1(SWSerial1); // Use SWSerial as the serial port.
 
 //motor constants
-bool motorEnable = true;
 float motorEffort = 150; //from -126 to 126
 
 //const float EFFORTRANGE = 126.0;
 const float EFFORTRANGE = 1.0;
 float effortP = 0.2;
 
+//Numpad Setup
+const byte ROWS = 4;
+const byte COLS = 4;
 
-//all input buttons have a pull down resistor
-const int firePin = 2;
-const int ignitionPin = 50;
-bool fire, ignition;
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
 
+Keypad numKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
+//LCD Setup
+LiquidCrystal_I2C lcd(0x27, 16, 2);  
+
+//inputs
+const int firePin = 12;
+const int motorEngagedPin = 13;
+bool fire, motorEnable;
 
 //print values
 float timeLast = millis();
 float timeCurr = timeLast;
 const float printTime = 300;
 
+//misc variables
+char lastKeyPress;
+
+char daysOfTheWeek[7][12] = {
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+};
+
 void setup()
 {
   Serial.begin(115200);
   
+  lcd.init();
+  lcd.backLight();
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1);
+  }
+
+  //rtc.adjust(DateTime(2023, 1, 14, 1, 18, 0)); //only use once sets to Jan 14, 2023, 00:10:00
+
+  pinMode(motorEngagedPin, INPUT);
+  pinMode(firePin, INPUT);
+
   //enable serial
   SWSerial1.begin(9600);
-  
 
 }
 
@@ -45,14 +91,20 @@ void loop()
   //////////////////////////////////////////
 
   //switches
-  ignition = digitalRead(ignitionPin);
+  motorEnable = digitalRead(motorEngagedPin);
   fire = digitalRead(firePin);
-
+  char numKey = numKeypad.getKey();
+  if(numKey) lastKeyPress = numKey; //store last numberpad key;
   //////////////////////////////////////////
-  ////////  ignition state machine    //////
+  ////////       state machine        //////
   //////////////////////////////////////////
-  if(ignition && fire) motorEnable = true;
-  else if(!ignition) motorEnable = false;
+  if(!motorEnable){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Motor Disabled");
+    lcd.setCursor(0, 1);
+    lcd.print("Reenable to Operate");
+  }
   
 
   //////////////////////////////////////////
@@ -80,13 +132,15 @@ void loop()
   if(timeCurr - timeLast > printTime){ //prints every printTime ms
 
     //switches
-    Serial.print("\tIgnition?: ");
-    Serial.print(ignition);
+    Serial.print("\tMotor Disabled??: ");
+    Serial.print();
     Serial.print("\tFire Missiles?: ");
     Serial.print(fire);
     Serial.print("\tmotor enable?: ");
     Serial.print(motorEnable);
     Serial.print("\n");
+    Serial.print("\tLast Keypress: ");
+    Serial.print(lastKeyPress);
 
     //values
     Serial.print("\tmotor effort: ");
@@ -96,4 +150,14 @@ void loop()
     timeLast = timeCurr;
   }
   
+}
+
+// returns array that is formatted [year, month, day, day of week, hour, minute, second]
+Struct DateAndTime(){
+  DateTime now = rtc.now();
+  Serial.print("Date & Time: " + now.year() + '/' + now.month() + 
+    '/' + now.day() + " (" + daysOfTheWeek[now.dayOfTheWeek()] + 
+      ") " + now.hour() + ':' + now.minute() + ':' + now.second());
+  return([now.year(), now.month(), now.day(), daysOfTheWeek[now.dayOfTheWeek()], 
+    now.hour(), now.minute(), now.second()]);
 }
